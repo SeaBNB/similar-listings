dbf0c67573f69de9422730ff0cd898deec02d7ed
Moriah Rahamim <moriahfaye@gmail.com>
2018-04-23 17:32:30 -0700

    achieve over 90% test coverage

--- spec/similarListings.spec.js
+++ spec/similarListings.spec.js
@@ -1,16 +1,45 @@
+import axios from 'axios';
 import React from 'react';
 import { shallow, mount, render } from 'enzyme'; 
 import { shallowToJson } from 'enzyme-to-json';
 
-jest.mock('axios');
-
 import SimilarListings from '../client/src/similarListings';
 import Listing from '../client/src/listing';
 
-describe('Similar Listings', () => {
-  const wrapper = mount(<SimilarListings id={0} />);
+jest.mock('axios');
 
-  test('renders three Listing components', () => {
-    expect(true).toBe(true);
-  }); 
+const listing = {
+  "url":"mockUrl",
+  "title":"nostrum laboriosam in rerum officiis",
+  "type":"Shared Room",
+  "numBeds":2,
+  "price":810,
+  "numRatings":965,
+  "avgStars":0.5,
+  "thumbnailImage":"mockImageUrl"
+};
+const fakeData = [];
+for (let i = 0; i < 10; i++) {
+  const newListing = Object.assign({id: i}, listing);
+  fakeData.push(newListing);
+}
+
+
+describe('Similar Listings', () => {
+  axios.get.mockResolvedValue({ data: fakeData});
+
+  test('fetches data with axios', () => {
+    const wrapper = mount(<SimilarListings id={0} />);
+    expect(axios.get.mock.calls.length).toBe(1);
+  });
+
+  test('creates a div with class "similar-listings"', () => {
+    const wrapper = shallow(<SimilarListings id={0} />);
+    expect(wrapper.find('.similar-listings').length).toBe(1);
+  });
+
+  test('creates a div with class "similar-listings-ribbon"', () => {
+    const wrapper = shallow(<SimilarListings id={0} />);
+    expect(wrapper.find('.similar-listings-ribbon').length).toBe(1);
+  });
 });
